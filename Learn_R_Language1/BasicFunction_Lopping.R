efa229658a40e37e0c83528ec33f66dda63ccf45
fachriezal <fachriezal.np@gmail.com>
2020-04-07 16:45:19 +0700

    belajar datascience R language 2

---
+++ Learn_R_Language1/BasicFunction_Lopping.R
@@ -0,0 +1,67 @@
+a<-20
+v = 5.7
+5 + 10
+b = 5.7
+c <- 5+6.2 - 0/1
+c
+4 ^ 2
+a + b
+a * c
+b2 = a*b
+b2
+"nama"
+n = "nama"
+n
+a2 <- paste ("nama", "saya")
+a2
+benarkah = 10 + 2 < 12
+benarkah
+10 / 0
+-10 / 0
+aa <- NA
+bb <- NaN
+c <- 100/0
+
+if(a==20)
+{
+  print("ini adalah angka")
+}else if(a>0){
+  print("bukan 20")
+}else{
+  print("negatif")
+}
+d <- 1
+while(i<10){
+  print("halo")
+  d <- d + 1
+}
+
+for (c in 1:10){
+  print(i)
+}
+
+
+luas_lingkaran <- function(r){
+  pi <- 3.14
+  luas <- pi*r^2
+  return(luas)
+}
+
+luas_segitiga <- function(alas,tinggi){
+  luas <- alas*tinggi/2
+  return(luas)
+}
+
+hw <- functiomns
+{
+  print("helllo wold")
+}
+lima <- function()
+{
+  print(5)
+}
+
+berhitung <- function(p)
+  for(i in 1:n){
+    print(i)
+  }
